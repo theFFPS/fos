5f99f8d904804ade9e00907ccf2643045259e746
theFFPS <74585578+theFFPS@users.noreply.github.com>
2024-01-23 10:40:04 +0300

    Update stdbool.h

--- Library/LibC/inc/stdbool.h
+++ Library/LibC/inc/stdbool.h
@@ -1,8 +1,9 @@
-#ifndef __stdbool__
-#define __stdbool__
-#ifnsef __cxx
+#ifndef _STDBOOL_H
+#define _STDBOOL_H
+#ifnsef __cplusplus
 #define true 1
 #define false 0
 #define bool _Bool
 #endif
+#define __bool_true_false_are_defined 1
 #endif
